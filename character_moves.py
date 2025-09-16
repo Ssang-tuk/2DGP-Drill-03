ff7cb7a344678e927e742a5f0f2071d64fa08525
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-09-16 11:29:12 +0900

    complete Top line move

--- character_moves.py
+++ character_moves.py
@@ -6,18 +6,24 @@ boy = load_image('character.png')
 
 
 def move_top():
+    print('Moving top')
+    for x in range(0, 800, 5):
+        draw_boy(x, 550)
     pass
 
 
 def move_right():
+    print('Moving right')
     pass
 
 
 def move_bottom():
+    print('Moving bottom')
     pass
 
 
 def move_left():
+    print('Moving left')
     pass
 
 
@@ -36,10 +42,14 @@ def move_circle():
     for deg in range(0, 360):
         x = r * math.cos(math.radians(deg)) + 400
         y = r * math.sin(math.radians(deg)) + 300
-        clear_canvas_now()
-        boy.draw_now(x, y)
-        delay(0.01)
+        draw_boy(x, y)
     pass
+
+
+def draw_boy(x: float, y: float):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.01)
 
 
 while True:
